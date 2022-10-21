97dd873211d6236328e43e0cc33842ad6f2d6c8b
vita <vita@DESKTOP-SANLI87>
2022-10-21 15:08:18 -0700

    Add project files.

---
+++ Assignment2/Controllers/J1Controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class J1Controller : ApiController
+    {
+        /// <summary>
+        /// Total Calories of the meal
+        /// </summary>
+        /// <param name="burger">Integer representing the index burger choice</param>
+        /// <param name="drink">Integer representing the index drink choice</param>
+        /// <param name="side">Integer representing the index side choice</param>
+        /// <param name="dessert">Integer representing the index dessert choice</param>
+        /// <returns>Your total calorie count is</returns>
+        /// <example>
+        /// Get:api/J1/Menu/4/4/4/4--> "Your total calorie count is 0"
+        /// Get:api/J1/Menu/1/2/3/4--> "Your total calorie count is 691"
+        /// </example>
+        [HttpGet]
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        
+        public string Menu (int burger, int drink, int side, int dessert)
+        {
+            List<int> burgerCal = new List<int> { 461, 431, 420, 0 };
+            burger = burgerCal[burger - 1];
+
+            List<int> drinkCal = new List<int> { 130, 160, 118, 0 };
+            drink = drinkCal[drink- 1];
+
+            List<int> sideCal = new List<int> { 100, 57, 70, 0 };
+            side = sideCal[side - 1];
+
+            List<int> dessertCal = new List<int> { 167, 266, 75, 0 };
+            dessert = dessertCal[dessert - 1];
+
+            int TotalCalorie = burger + drink + side + dessert;
+            return "Your total calorie count is " + TotalCalorie;
+        }
+    }
+}
---
+++ Assignment2/Controllers/J3Controller.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class J3Controller : ApiController
+    {
+        /// <summary>
+        /// juniorEF2020 Problem J3:Art
+        /// </summary>
+        /// <param name="N">number of drops of paint</param>
+        /// <param name="X">the coordinate X of a drop of paint on the canvas.</param>
+        /// <param name="Y">the coordinate Y of a drop of paint on the canvas</param>
+        /// <returns>
+        /// The first line represents the coordinates of the bottom-left corner of the rectangular frame.
+        /// The second line represents the coordinates of the top-right corner of the rectangular frame.</returns>
+        /// comment: I don't know how to gather more than one input at the same time.
+        [HttpGet]
+        [Route("api/J3/Art/{N}/{X}/{Y}")]
+        public IEnumerable <string> Get (int N, int[] X, int[] Y)
+        {
+
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int d = 0;
+            
+            for (int i = 0; i <N; i++)
+            {
+                
+                a = Math.Min(Math.Min(X[i], X[i+1]), Math.Min(X[i], X[i + 2])) - 1;
+                b = Math.Min(Math.Min(Y[i], Y[i + 1]),Math.Min(Y[i], Y[i+2]))- 1;
+                c = Math.Max(Math.Max(X[i], X[i+1]), Math.Max(X[i], X[i + 2])) + 1;
+                d = Math.Max(Math.Max(Y[i], Y[i+1]), Math.Max(Y[i], Y[i+2])) + 1;
+       
+                
+            }
+            return new string[] { a + "," + b, c + "," + d };
+
+
+
+        }
+    }
+}
